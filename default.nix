15b8457333384b7ac93b17452602eeef8f6603cf
CrackTC <chenrke369@gmail.com>
2023-09-14 21:43:50 +0800

    add libjportaudio

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  libjportaudio = pkgs.callPackage ./pkgs/libjportaudio { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
---
+++ pkgs/libjportaudio/default.nix
@@ -0,0 +1,42 @@
+{ stdenv
+, fetchFromGitHub
+, oraclejdk8
+, cmake
+, portaudio
+}:
+
+let
+  version = "0.1.0";
+  pname = "libjportaudio";
+  jdk = oraclejdk8.overrideAttrs {
+    src = fetchTarball {
+      url = "https://static.sora.zip/nix/jdk-8u281-linux-x64.tar.gz";
+      sha256 = "0f9fb37p75cf7qfm67yc8ariqksnw8641kh2zcwvlrr4r8lgj70v";
+    };
+  };
+in
+stdenv.mkDerivation rec {
+  inherit version pname;
+  name = "${pname}-${version}";
+  src = fetchFromGitHub ({
+    owner = "philburk";
+    repo = "portaudio-java";
+    rev = "2ec5cc47d6f8abe85ddb09c34e69342bfe72c60b";
+    sha256 = "t+Pqtgstd1uJjvD4GKomZHMeSECNLeQJOrz97o+lV2Q=";
+  });
+
+  nativeBuildInputs = [ cmake portaudio ];
+
+  preConfigure = ''
+    export JAVA_HOME=${jdk}
+  '';
+
+  buildPhase = ''
+    make jportaudio_0_1_0
+  '';
+
+  installPhase = ''
+    mkdir -p $out/lib
+    cp -r libjportaudio_0_1_0.so $out/lib/libjportaudio.so
+  '';
+}
