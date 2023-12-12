be48d25dfc850d1555c81d31b747968670a3467a
CrackTC <chenrke369@gmail.com>
2023-12-12 16:28:10 +0800

    bbdown: fix dep

--- pkgs/bbdown/default.nix
+++ pkgs/bbdown/default.nix
@@ -1,22 +1,31 @@
 { lib
 , buildDotnetGlobalTool
 , dotnetCorePackages
+, ffmpeg
 }:
 
 let
   pname = "BBDown";
   version = "1.6.1";
   dotnet-sdk = dotnetCorePackages.sdk_8_0;
-in
-buildDotnetGlobalTool {
-  inherit pname version dotnet-sdk;
-  nugetSha256 = "sha256-FujKRBiuvbndxPo/SF7dOQbRqLr85mYl9Kay0W+CvkU=";
+  dotnet-runtime = dotnet-sdk;
+  bbdown = buildDotnetGlobalTool
+    {
+      inherit pname version dotnet-runtime dotnet-sdk;
+      nugetSha256 = "sha256-FujKRBiuvbndxPo/SF7dOQbRqLr85mYl9Kay0W+CvkU=";
 
-  meta = with lib; {
-    description = "Bilibili Downloader";
-    homepage = "https://github.com/nilaoda/BBDown";
-    license = licenses.mit;
-    mainProgram = "BBDown";
-    platforms = [ "x86_64-linux" ];
-  };
-}
+      meta = with lib; {
+        description = "Bilibili Downloader";
+        homepage = "https://github.com/nilaoda/BBDown";
+        license = licenses.mit;
+        mainProgram = pname;
+        platforms = platforms.linux;
+      };
+    };
+in
+bbdown.overrideAttrs (attrs: {
+  postFixup = ''
+    wrapProgram $out/bin/${pname} \
+      --prefix PATH : "${lib.makeBinPath [ ffmpeg ]}"
+  '';
+})
