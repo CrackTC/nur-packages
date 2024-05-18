683bfd037e1fad32535219bf1077739ba6d7a12b
CrackTC <chenrke369@gmail.com>
2024-05-18 21:34:25 +0800

    pyink: pin deps

--- pkgs/pyink/default.nix
+++ pkgs/pyink/default.nix
@@ -12,5 +12,16 @@
     hash = "sha256-CsQYh1Rh/Kc7S6hbSaa0WYrM2+AD7x8xxcXXLCo1Tdo=";
   };
   format = "pyproject";
-  propagatedBuildInputs = [ hatchling hatch-vcs black ];
+  propagatedBuildInputs = [
+    hatchling
+    hatch-vcs
+    (black.overrideAttrs (super: {
+      version = "24.3.0";
+      src = fetchPypi {
+        inherit (super) pname;
+        inherit version;
+        sha256 = "sha256-oMnEoHca/GkZV4zscc6Co+MeBUkE5xl96svJOCZxxB8=";
+      };
+    }))
+  ];
 }
